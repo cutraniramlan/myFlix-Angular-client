f4e7492f8b928002ea6940c7b554ecd21c176518
cutraniramlan <cutraniramlan@gmail.com>
2023-03-08 10:51:13 +0100

    update

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,50 +1,95 @@
-import { Component, OnInit, Input } from '@angular/core';
+// import { Component, OnInit, Input } from '@angular/core';
 
-// You'll use this import to close the dialog on success
-import { MatDialogRef } from '@angular/material/dialog';
+// // You'll use this import to close the dialog on success
+// import { MatDialogRef } from '@angular/material/dialog';
 
-// This import brings in the API calls we created in 6.2
-import { FetchApiDataService } from '../fetch-api-data.service';
+// // This import brings in the API calls we created in 6.2
+// import { FetchApiDataService } from '../fetch-api-data.service';
 
-// This import is used to display notifications back to the user
-import { MatSnackBar } from '@angular/material/snack-bar';
+// // This import is used to display notifications back to the user
+// import { MatSnackBar } from '@angular/material/snack-bar';
 
-import { Router } from '@angular/router';
+// import { Router } from '@angular/router';
 
 
+// @Component({
+//   selector: 'app-user-login-form',
+//   templateUrl: './user-login-form.component.html',
+//   styleUrls: ['./user-login-form.component.scss']
+// })
+// export class UserLoginFormComponent implements OnInit {
+
+//   @Input() userData = { Username: '', Password: '' };
+
+// constructor(
+//     public fetchApiData: FetchApiDataService,
+//     public dialogRef: MatDialogRef<UserLoginFormComponent>,
+//     public snackBar: MatSnackBar,
+//     private router: Router) { }
+
+// ngOnInit(): void {
+// }
+
+// // This is the function responsible for sending the form inputs to the backend
+// loginUser(): void {
+//   this.fetchApiData.userLogin(this.userData).subscribe(
+//     (result) => {
+//       console.log(result);
+//       localStorage.setItem('user', result.user.Username);
+//       localStorage.setItem('token', result.token);
+//       this.dialogRef.close(); // This will close the modal on success
+//       this.router.navigate(['movies']);
+//     },
+//     (result) => {
+//       this.snackBar.open(result, 'OK', {
+//         duration: 2000,
+//       });
+//     }
+//   );
+// }
+// }
+
+import { Component, OnInit, Input } from "@angular/core";
+import { MatDialogRef } from "@angular/material/dialog";
+import { FetchApiDataService } from "../fetch-api-data.service";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Router } from "@angular/router";
 @Component({
-  selector: 'app-user-login-form',
-  templateUrl: './user-login-form.component.html',
-  styleUrls: ['./user-login-form.component.scss']
+  selector: "app-user-login-form",
+  templateUrl: "./user-login-form.component.html",
+  styleUrls: ["./user-login-form.component.scss"],
 })
 export class UserLoginFormComponent implements OnInit {
-
-  @Input() userData = { Username: '', Password: '' };
-
-constructor(
+  @Input() userData = { Username: "", Password: "" };
+  constructor(
+    private router: Router,
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar,
-    private router: Router) { }
+    public snackBar: MatSnackBar
+   ) {}
+   ngOnInit(): void {}
 
-ngOnInit(): void {
-}
+   /**
+    * Send the form inputs to the backend via API call
+   
+    */
 
-// This is the function responsible for sending the form inputs to the backend
-loginUser(): void {
-  this.fetchApiData.userLogin(this.userData).subscribe(
-    (result) => {
-      console.log(result);
-      localStorage.setItem('user', result.user.Username);
-      localStorage.setItem('token', result.token);
-      this.dialogRef.close(); // This will close the modal on success
-      this.router.navigate(['movies']);
-    },
-    (result) => {
-      this.snackBar.open(result, 'OK', {
-        duration: 2000,
-      });
-    }
-  );
-}
+   logInUser(): void {
+     this.fetchApiData.userLogin(this.userData).subscribe(
+       (result) => {
+        localStorage.setItem("token", result.token);
+        localStorage.setItem("user", result.user.Username);
+        this.router.navigate(["movies"]);
+        this.dialogRef.close();
+        this.snackBar.open(`${result.user.Username} has logged in!`, "OK", {
+          duration: 2000,
+        });
+      },
+      (result) => {
+        this.snackBar.open(result, "OK", {
+          duration: 2000,
+        });
+      }
+    );
+  }
 }
